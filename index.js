8ef7f824c8a45c701858720e09b0fcb538792091
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-06-21 08:26:27 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',function(){
+   let change = document.getElementById('text')
+   change.innerHTML = "This is really cool!"
+})
+console.log('This console.log() fires when index.js loads - before DOMContentLoaded is triggered');
